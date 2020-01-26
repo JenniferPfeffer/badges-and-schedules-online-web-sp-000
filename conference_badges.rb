683232438784aa1e798b9558c12c11fe6b0b1dc4
Jennifer Pfeffer <jennifer.pfeffer127@gmail.com>
2020-01-26 16:51:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,9 +7,9 @@ def batch_badge_creator(attendees)
   attendees.each {|name| badges.push("Hello, my name is #{name}.")}
     return badges
 end
-def assign_rooms(attendees,room) 
+def assign_rooms(attendees) 
   list = [] 
-  attendees.each_with_index { |name| list.push("Hello, #{name}! You will be assigned to room #{index + 1}!")}
+  attendees.each_with_index.map { |name,index| list.push("Hello, #{name}! You will be assigned to room #{index + 1}!")}
   return list 
 end 
 def printer(batch_badge_creator,assign_rooms) 
